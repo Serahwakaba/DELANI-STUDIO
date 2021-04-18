0b90af929108433c42f91f27f6758b8d149668a1
Serah wakaba <serahwakaba9@gmail.com>
2021-04-18 15:55:10 +0300

    jquery submit function complete

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,16 @@ $(".product").click(function () {
     $("#product-p").toggle();
     $("#product").toggle();
 });
+
+$("#submit").click(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var mail = $("mail").val();
+    var text = $("#text").val();
+
+ if (name == "" || mail == "" || text == "") {
+        alert("hi" + " " + name + " " + "we have recieved Your message .Thank you for reaching out to us")
+    } else {
+        alert("please make sure you have filled in the form correctly")
+    }
+})
